2bcc9b1142a9ed57e92a81e1f3279b129ce7fa5f
Benjamin-Re <renker.benjamin@gmail.com>
2022-08-17 20:38:40 +0200

    Add imgUrl, read data from external Json, add discount price display

--- components/card.tsx
+++ components/card.tsx
@@ -1,17 +1,25 @@
 import styles from './card.module.css';
 import Image from 'next/image';
-export default function Card() {
+
+export default function Card(props) {
   return (
     <>
       <div className={styles.card}>
         <Image
-          src="/images/camiseta-2.jpg" // Route of the image file
+          src={'/images/' + props.imgUrl} // Route of the image file
           height={344} // Desired size with correct aspect ratio
           width={244} // Desired size with correct aspect ratio
           alt="Camiseta con mangas largas"
         />
-        <div className={styles.description}>Camiseta con mangas largas</div>
-        <div className={styles.price}>22.99€</div>
+        <div className={styles.description}>{props.description}</div>
+        {props.discount ? (
+          <>
+            <div className={styles.oldPrice}>{props.price}€</div>
+            <div className={styles.discountPrice}>{props.price*((100-props.discount)/100)}€(-{props.discount}%)</div>
+          </>
+        ) : (
+          <div className={styles.price}>{props.price}€</div>
+        )}
         <div className={styles.moreColors}>más colores</div>
         <button className={styles.addBtn}>AÑADIR</button>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,42 @@
 import { NextPage } from 'next';
-import  Card  from "../../components/card"
+import Card from '../../components/card';
 
-const HomePage: NextPage = () => {
-  return (<>
-  <Card></Card>
-  <Card></Card>
-  <Card></Card>
-  <Card></Card>
-  <Card></Card>
-  <Card></Card>
-  </>)
+interface Products {
+  products: Product[]
+}
+
+interface Product {
+  id: number,
+  description: string,
+  price: number,
+  imgUrl: string,
+  discount: number
+}
+ 
+const HomePage: NextPage = (props: Products) => {
+  const products = props.products
+  console.log(products)
+  return (
+    <>
+      {products?.map(product => {
+        return (
+          <Card {...product}></Card>
+        )
+      })}
+    </>
+  );
 };
 
 export default HomePage;
+
+import path from 'path'
+import fsPromises from 'fs/promises'
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), '/public/products.json')
+  const jsonData = await fsPromises.readFile(filePath)
+  const objectData = JSON.parse(jsonData.toString())
+  return {
+    props: objectData
+  }
+}
